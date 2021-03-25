3cdc5148f2e93f41264fe319abcbfacb8dfe3982
Patricia <patysiq@yahoo.com.br>
2021-03-25 09:19:13 -0300

    add unit test

--- src/main/java/com/validadorCpf/controllers/HomeController.java
+++ src/main/java/com/validadorCpf/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package com.validadorCpf.controllers;
 
+import com.validadorCpf.models.Cliente;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +12,12 @@ public class HomeController {
 	@GetMapping("/")
 	public String index(){
 		return "home/index";
+	}
+
+	@GetMapping("/valida-cpf")
+	public String validaCpf(Cliente cliente, Model model){
+		boolean verdade = cliente.validarCpf();
+		model.addAttribute("verdade", verdade);
+		return "home/cpfValidado";
 	}
 }
--- src/test/java/com/validadorCpf/ValidadorCpfApplicationTests.java
+++ src/test/java/com/validadorCpf/ValidadorCpfApplicationTests.java
@@ -1,5 +1,7 @@
 package com.validadorCpf;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.validadorCpf.models.Cliente;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,74 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ValidadorCpfApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void fazendoTesteDeCpfValido() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("72434467016");
+		assertEquals(true, cliente.validarCpf());
 	}
 
+	@Test
+	void fazendoTesteDeCpfValidoComDigito() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("724.344.670-16");
+		assertEquals(true, cliente.validarCpf());
+	}
+
+	@Test
+	void fazendoTesteDeCpfValidoComEspacoNoMeio() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("724344 67016");
+		assertEquals(true, cliente.validarCpf());
+	}
+
+	@Test
+	void fazendoTesteDeCpfValidoComEspacoNoComeco() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf(" 72434467016");
+		assertEquals(true, cliente.validarCpf());
+	}
+
+	@Test
+	void fazendoTesteDeCpfInvalidoComDigito() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("724.344.670-17");
+		assertEquals(false, cliente.validarCpf());
+	}
+
+	@Test
+	void fazendoTesteDeCpfInvalido() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("72434467017");
+		assertEquals(false, cliente.validarCpf());
+	}
+
+	@Test
+	void fazendoTesteDeCpfComDigitoAMenos() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("7243446");
+		assertEquals(false, cliente.validarCpf());
+	}
+
+    @Test
+	void fazendoTesteDeCpfComCaracteres() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("7243446,,7016");
+		assertEquals(false, cliente.validarCpf());
+	}
+
+    @Test
+	void fazendoTesteDeCpfComLetras() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Patricia");
+        cliente.setCpf("724344AA016");
+		assertEquals(false, cliente.validarCpf());
+	}
 }
